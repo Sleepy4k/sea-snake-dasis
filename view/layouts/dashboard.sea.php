3da5f561d8bf6c1a6e691d721c96484e0cde935c
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-11-10 17:30:57 +0700

    chore(layout): slicing some tag

--- view/layouts/dashboard.sea.php
+++ view/layouts/dashboard.sea.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="en" oncontextmenu="return false">
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?= $this->put('partials.head.meta') ?>
 
-    <title><?= config('bin', 'name') ?> | <?= $get('title') ?></title>
+    <?= $this->put('partials.head.title') ?>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= asset('css/dashboard.css') ?>">
+    <?= $this->section('css') ?>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+      <link rel="stylesheet" href="<?= asset('css/dashboard.css') ?>">
+    <?= $this->show() ?>
   </head>
   <body>
     <?= $this->put('partials.header.dashboard') ?>
 
     <div class="container-fluid">
       <div class="row">
-        <?= $this->put('partials.sidebar.dashboard') ?>
+        <?= $this->component('partials.sidebar.dashboard') ?>
+          <?= $this->get('sidebar') ?>
+        <?= $this->endcomponent() ?>
 
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
           <?= $this->get('content') ?>
@@ -24,7 +26,5 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
-    <script src="<?= asset('js/dashboard.js') ?>"></script>
   </body>
 </html>
--- view/layouts/landing.sea.php
+++ view/layouts/landing.sea.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?= $this->put('partials.head.meta') ?>
 
-    <title><?= config('bin', 'name') ?></title>
+    <?= $this->put('partials.head.title') ?>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= asset('css/landing.css') ?>">
